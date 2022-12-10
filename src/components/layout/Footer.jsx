d892bc3a3ac775f423fd442027f0b6886a54c8c6
farrahtor <farrahtor@gmail.com>
2022-12-10 21:51:25 +0100

    ajout html css de header et footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,5 +2,16 @@ import React from "react";
 import "../../assets/sass/layout/Footer.scss";
 
 export const Footer = () => {
-  return <footer>Je suis le footer</footer>;
+  return (
+    <footer className="App-footer">
+      <div className="App-footer-bloc">
+        <div>
+          <img src="../LOGO2.svg" alt="Logo Kaza"></img>
+        </div>
+        <div>
+          <p>© 2020 Kasa. All rights reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,5 +2,17 @@ import React from "react";
 import "../../assets/sass/layout/Header.scss";
 
 export const Header = () => {
-  return <header id="App-header">Je suis le header</header>;
+  return (
+    <header className="App-header">
+      <div className="App-header-logo">
+        <img src="../LOGO.svg" alt="Logo Kaza"></img>
+      </div>
+      <nav className="App-header-nav">
+        <ul>
+          <li>Accueil{/* <Link to="about">Home</Link> */}</li>
+          <li>A Propos</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
