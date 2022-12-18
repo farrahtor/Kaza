f627bb080b11a118cabc7e02fce404b280064435
farrahtor <farrahtor@gmail.com>
2022-12-18 23:42:09 +0100

    add function slidshow

--- src/components/desktop/Slideshow.jsx
+++ src/components/desktop/Slideshow.jsx
@@ -2,39 +2,58 @@ import React, { useState } from "react";
 
 import "../../assets/sass/desktop/Slideshow.scss";
 
-export const Slideshow = ({ id, pictures = [] }) => {
-  const [active, setActive] = useState("");
-  const arrowClick = (e) => {
-    console.log(setActive);
-    setActive(!active);
+export const Slideshow = ({ pictures = [] }) => {
+  const [index, setIndex] = useState(0);
+
+  const nextClick = (e) => {
+    if (index === pictures.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
   };
-  const pictureLength = pictures.length;
-  console.log(pictureLength);
+
+  const backClick = (e) => {
+    if (index === 0) {
+      setIndex(index + 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
 
   return (
-    <div className="Slideshow">
-      <img
-        className="Slideshow-icon arrow-next"
-        src="../arrow.svg"
-        alt="next"
-        onClick={arrowClick}
-      />
-      <img
-        className="Slideshow-icon arrow-back"
-        src="../arrow.svg"
-        alt="back"
-        onClick={arrowClick}
-      />
-      <div className="Slideshow-container">
-        {pictures.map((picture, index) => (
-          <img
-            key={index}
-            className="Slideshow-container-img"
-            src={picture}
-            alt=""
+    <div className="slideshow">
+      <span className="slideshow-icon arrow-next" onClick={nextClick}>
+        <svg
+          width="16"
+          height="10"
+          viewBox="0 0 16 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M13.8495 9.1944L15.2079 7.8283L7.60994 0.238037L0.0120024 7.83598L1.37042 9.1944L7.60994 2.95488L13.8495 9.1944V9.1944Z"
+            fill="white"
           />
-        ))}
-      </div>
+        </svg>
+      </span>
+
+      <span className="slideshow-icon arrow-back" onClick={backClick}>
+        <svg
+          width="16"
+          height="10"
+          viewBox="0 0 16 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M13.8495 9.1944L15.2079 7.8283L7.60994 0.238037L0.0120024 7.83598L1.37042 9.1944L7.60994 2.95488L13.8495 9.1944V9.1944Z"
+            fill="white"
+          />
+        </svg>
+      </span>
+
+      <img className="slideshow-img" src={pictures[index]} alt="" />
     </div>
   );
 };
